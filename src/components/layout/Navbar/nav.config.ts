b6d63e336888e0216fe2be0c266fb791f2edd14a
adityaj698 <adityaj698@gmail.com>
2025-12-26 16:19:09 +0530

    dashboard screen

--- src/components/layout/Navbar/nav.config.ts
+++
@@ -1,6 +0,0 @@
-export const NAV_TABS = [
-  { label: "Home", path: "/" },
-  { label: "About", path: "/about" },
-  { label: "Delivery", path: "/delivery" },
-  { label: "Contact Us", path: "/contact" },
-] as const;
